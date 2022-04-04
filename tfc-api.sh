48c2eba9406077c0b09d8e18c02c44276e9b1a7e
devhulk <gry2big93@gmail.com>
2022-04-04 10:02:23 -0400

    changed script

--- tfc-api.sh
+++ tfc-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Step 1 - Create upload file (is an archive file ending in .tar.gz)
-UPLOAD_FILE_NAME="./tf-$(date +%s).tar.gz"
+UPLOAD_FILE_NAME="./content-$(date +%s).tar.gz"
 
 tar -zcvf "$UPLOAD_FILE_NAME" -C "$CONTENT_DIRECTORY" .
 echo $UPLOAD_FILE_NAME
@@ -23,7 +23,7 @@ echo "Creating Configuration Version"
 
 # Step 3 - Create config_version. This tracks your terraform configuration version.
 
-echo '{"data":{"type":"configuration-versions", "attributes": "auto-queue-runs": true}}' > ./create_config_version.json
+echo '{"data":{"type":"configuration-versions"}}' > ./create_config_version.json
 
 
 echo "Getting Upload URL for Config Version"
@@ -50,3 +50,7 @@ curl \
   --request PUT \
   --data-binary @"$UPLOAD_FILE_NAME" \
   $UPLOAD_URL
+
+# Clean Up 
+# rm content-*
+# rm create_config_version.json
